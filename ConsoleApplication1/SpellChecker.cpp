343e0e2a05bff967a001fcd67bd4db624727c8c8
mynamejason <ykim4141@gmail.com>
2020-08-16 15:39:15 -0400

    implemented separate suggestions

--- ConsoleApplication1/SpellChecker.cpp
+++ ConsoleApplication1/SpellChecker.cpp
@@ -33,6 +33,7 @@ void SpellChecker::checkSpelling() {
 }
 
 void SpellChecker::insertion(const std::string& currentIncorrectWord) {
+	std::string tempAns = "";
 	for (int wordPos = 0; wordPos <= currentIncorrectWord.size(); wordPos++) {
 		for (int c = 97; c < 123; c++) {
 			std::string charToString(1, c);
@@ -41,27 +42,50 @@ void SpellChecker::insertion(const std::string& currentIncorrectWord) {
 			for (int i = 0; i < dictionary_.size(); i++) {
 				if (currentWord == dictionary_[i]) {
 					fixedWords_.push_back(currentWord);
+					tempAns += currentWord;
+					tempAns += ", ";
 					break;
 				}
 			}
 		}
 	}
+	if(tempAns.size() > 0){
+		std::cout << "(";
+		if(tempAns[tempAns.size() - 1] == ' '){
+			tempAns.erase(tempAns.begin() + tempAns.size() - 2, tempAns.begin() + tempAns.size());
+		}
+		std::cout << tempAns;
+		std::cout << ")" << std::endl;
+	}
+
 }
 
 void SpellChecker::deletion(const std::string& currentIncorrectWord) {
+	std::string tempAns = "";
 	for (int wordPos = 0; wordPos < currentIncorrectWord.size(); wordPos++) {
 		std::string currentWord = currentIncorrectWord;
 		currentWord.erase(currentWord.begin() + wordPos);
 		for (int i = 0; i < dictionary_.size(); i++) {
 			if (currentWord == dictionary_[i]) {
-				fixedWords_.push_back(currentWord);
+				//fixedWords_.push_back(currentWord);
+				tempAns += currentWord;
+				tempAns += ", ";
 				break;
 			}
 		}
 	}
+	if(tempAns.size() > 0){
+		std::cout << "(";
+		if(tempAns[tempAns.size() - 1] == ' '){
+			tempAns.erase(tempAns.begin() + tempAns.size() - 2, tempAns.begin() + tempAns.size());
+		}
+		std::cout << tempAns;
+		std::cout << ")" << std::endl;
+	}
 }
 
 void SpellChecker::swap(const std::string& currentIncorrectWord) {
+	std::string tempAns = "";
 	for (int currentWordPos = 0; currentWordPos < currentIncorrectWord.size()-1; currentWordPos++) {
 		for (int i = currentWordPos; i < currentIncorrectWord.size()-1; i++) {
 			
@@ -77,15 +101,26 @@ void SpellChecker::swap(const std::string& currentIncorrectWord) {
 
 			for (int i = 0; i < dictionary_.size(); i++) {
 				if (currentWord == dictionary_[i]) {
-					fixedWords_.push_back(currentWord);
+					//fixedWords_.push_back(currentWord);
+					tempAns += currentWord;
+					tempAns += ", ";
 					break;
 				}
 			}
 		}
 	}
+	if(tempAns.size() > 0){
+		std::cout << "(";
+		if(tempAns[tempAns.size() - 1] == ' '){
+			tempAns.erase(tempAns.begin() + tempAns.size() - 2, tempAns.begin() + tempAns.size());
+		}
+		std::cout << tempAns;
+		std::cout << ")" << std::endl;
+	}
 }
 
 void SpellChecker::replace(const std::string& currentIncorrectWord) {
+	std::string tempAns = "";
 	for (int wordPos = 0; wordPos < currentIncorrectWord.size(); wordPos++) {
 		for (int c = 97; c < 123; c++) {
 			std::string currentWord = currentIncorrectWord;
@@ -93,10 +128,20 @@ void SpellChecker::replace(const std::string& currentIncorrectWord) {
 
 			for (int i = 0; i < dictionary_.size(); i++) {
 				if (currentWord == dictionary_[i]) {
-					fixedWords_.push_back(currentWord);
+					//fixedWords_.push_back(currentWord);
+					tempAns += currentWord;
+					tempAns += ", ";
 					break;
 				}
 			}
 		}
+	}
+	if(tempAns.size() > 0){
+		std::cout << "(";
+		if(tempAns[tempAns.size() - 1] == ' '){
+			tempAns.erase(tempAns.begin() + tempAns.size() - 2, tempAns.begin() + tempAns.size());
+		}
+		std::cout << tempAns;
+		std::cout << ")" << std::endl;
 	}
 }
